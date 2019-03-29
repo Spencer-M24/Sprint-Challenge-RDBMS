d1cd8941103f6972a3461ecdcb09842736eaeb11
Spencer Meredith <Smeredith1119@gmail.com>
2019-03-29 08:38:00 -0700

    index.js/knex  and setting up layout structure/

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    development: {
+
+        client: "sqlite3",
+
+        connection: { filename:  },
+
+        useNullAsDefault: true,
+
+        migrations: {
+
+            directory: 
+
+            tableName: 
+
+        },
+
+        seeds: { directory:  }
+    }
+};
